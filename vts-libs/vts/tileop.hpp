861538532d4d4fab4a4afe38f08973b9ac0fae77
Václav Blažek <vaclav.blazek@citationtech.net>
2015-08-18 15:59:55 +0000

    vadstena: updated metatile children order
    git-svn-id: https://scm.citationtech.net/trunk/vadstena@14014 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- vts-libs/vts/tileop.hpp
+++ vts-libs/vts/tileop.hpp
@@ -47,11 +47,12 @@ inline TileIdChildren children(const TileId &tileId)
 {
     TileId base(tileId.lod + 1, tileId.x << 1, tileId.y << 1);
 
+    // children must be from lower-left due to adapter
     return {{
-        base                                      // upper-left
-        , { base.lod, base.x + 1, base.y }        // upper-right
-        , { base.lod, base.x, base.y + 1 }        // lower-left
-        , { base.lod, base.x + 1, base.y + 1 }    // lower-right
+        { base.lod, base.x, base.y + 1 }        // lower-left
+        , { base.lod, base.x + 1, base.y + 1 }  // lower-right
+        , base                                  // upper-left
+        , { base.lod, base.x + 1, base.y }      // upper-right
     }};
 }
 
