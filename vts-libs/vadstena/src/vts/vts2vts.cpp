bd1408f86a01cb4daa0fa81ddba69dc0744acccc
Vaclav Blazek <vaclav.blazek@citationtech.net>
2016-01-27 18:08:41 +0100

    vts2vts: WIP

--- vts-libs/vadstena/src/vts/vts2vts.cpp
+++ vts-libs/vadstena/src/vts/vts2vts.cpp
@@ -125,82 +125,6 @@ usage
     return false;
 }
 
-std::map<std::string, math::Extents2>
-measure(const vts::TileSet &tileset, const vr::ReferenceFrame &dstRf)
-{
-    // initialize extents mapping
-    std::map<std::string, math::Extents2> mapping;
-    for (const auto &item : dstRf.division.nodes) {
-        mapping[item.second.srs] = math::Extents2(math::InvalidExtents{});
-    }
-
-    const auto &srcRf(tileset.referenceFrame());
-    traverse(tileset.tileIndex()
-             , [&](const vts::TileId &tid, vts::QTree::value_type flags)
-    {
-        if (!vts::TileIndex::Flag::isReal(flags)) { return; }
-
-        vts::NodeInfo ni(srcRf, tid);
-
-        // for each destination node
-        for (const auto &item : dstRf.division.nodes) {
-            const auto &node(item.second);
-            vts::CsConvertor csconv(ni.node.srs, node.srs);
-
-            auto dstExtents(csconv(ni.node.extents));
-            if (!overlaps(dstExtents, node.extents)) { continue; }
-            dstExtents = intersect(dstExtents, node.extents);
-
-            // update extents
-            auto &m(mapping[node.srs]);
-            update(m, dstExtents.ll);
-            update(m, dstExtents.ur);
-        };
-    });
-
-    for (const auto item : mapping) {
-        LOG(info4) << std::fixed << item.first << ": " << item.second;
-    }
-
-    return mapping;
-}
-
-class Encoder : public vts::Encoder {
-public:
-    Encoder(const boost::filesystem::path &path
-            , const vts::TileSetProperties &properties
-            , vts::CreateMode mode
-            , const vts::TileSet &input
-            , const Config &config)
-        : vts::Encoder(path, properties, mode)
-        , config_(config), input_(input), srcRf_(input_.referenceFrame())
-    {
-        auto extents(measure(input, referenceFrame()));
-
-        setConstraints(Constraints().setExtentsGenerator
-                       ([&](const std::string &srs) -> math::Extents2
-        {
-            return extents.at(srs);
-        }).setLodRange(vts::LodRange(0, 24)));
-
-        // FIXME: what lodRange???
-        // TODO: lodRange based on data
-    }
-
-private:
-    virtual TileResult
-    generate(const vts::TileId &tileId, const vts::NodeInfo &nodeInfo
-             , const TileResult&)
-        UTILITY_OVERRIDE;
-
-    virtual void finish(vts::TileSet&) UTILITY_OVERRIDE {}
-
-    const Config config_;
-
-    const vts::TileSet &input_;
-    const vr::ReferenceFrame srcRf_;
-};
-
 double triangleArea(const math::Point2 &a, const math::Point2 &b,
                     const math::Point2 &c)
 {
@@ -266,133 +190,233 @@ void forEachTile(const vr::ReferenceFrame &referenceFrame
     }
 }
 
-struct OutOfTree {};
-
-std::vector<vts::TileId>
-rasterTiles(const vr::ReferenceFrame &referenceFrame
-            , const vr::ReferenceFrame::Division::Node &rootNode
-            , vts::Lod lod, const vts::TileRange &tileRange)
+template <typename Op>
+void rasterizeTiles(const vr::ReferenceFrame &referenceFrame
+                    , const vr::ReferenceFrame::Division::Node &rootNode
+                    , vts::Lod lod, const vts::TileRange &tileRange
+                    , Op op)
 {
     // process tile range
+    forEachTile(referenceFrame, lod, tileRange
+                , [&](const vts::NodeInfo &ni)
+    {
+        LOG(info4)
+            << std::fixed << "dst tile: "
+            << ni.nodeId() << ", " << ni.node.extents;
+
+        // TODO: check for incidence with Q
+
+        // check for root
+        if (ni.subtreeRoot->id == rootNode.id) {
+            op(vts::tileId(ni.nodeId()));
+        }
+    });
+}
+
+typedef std::map<vts::TileId, vts::TileId::list> SourceInfo;
+
+math::Points2 projectCorners(const vr::ReferenceFrame::Division::Node &node
+                             , const vts::CsConvertor &conv
+                             , const math::Points2 &src)
+{
+    math::Points2 dst;
     try {
-        std::vector<vts::TileId> tiles;
-        forEachTile(referenceFrame, lod, tileRange
-                    , [&](const vts::NodeInfo &ni)
-        {
-            LOG(info4)
-                << std::fixed
-                << "src tile: "
-                << ni.nodeId() << ", " << ni.node.extents;
-
-            // TODO: check for incidence with Q
-
-            // check for root
-            if (ni.subtreeRoot->id != rootNode.id) {
-                LOG(warn2)
-                    << "Node " << ni.nodeId() << " is not directly under"
-                    << " currently processed node " << rootNode.id << ".";
-                throw OutOfTree{};
+        for (const auto &c : src) {
+            dst.push_back(conv(c));
+            LOG(info4) << std::fixed << "corner: "
+                       << c << " -> " << dst.back();
+            if (!inside(node.extents, dst.back())) {
+                // projected dst tile cannot fit inside this node's
+                // extents -> ignore
+                return {};
             }
+        }
+    } catch (std::exception) {
+        // whole tile cannot be projected -> ignore
+        return {};
+    }
 
-            tiles.push_back(vts::tileId(ni.nodeId()));
+    // OK, we could convert whole tile into this reference system
+    return dst;
+}
+
+class SourceInfoBuilder : boost::noncopyable {
+public:
+    SourceInfoBuilder(const vts::TileSet &tileset
+                      , const vr::ReferenceFrame &dstRf)
+    {
+        const auto &srcRf(tileset.referenceFrame());
+        traverse(tileset.tileIndex()
+                 , [&](const vts::TileId &srcId, vts::QTree::value_type flags)
+        {
+            if (!vts::TileIndex::Flag::isReal(flags)) { return; }
+
+            vts::NodeInfo ni(srcRf, srcId);
+
+            const auto &srcExtents(ni.node.extents);
+            const math::Points2 srcCorners = {
+                ul(srcExtents), ur(srcExtents), lr(srcExtents), ll(srcExtents)
+            };
+
+            // for each destination node
+            for (const auto &item : dstRf.division.nodes) {
+                const auto &node(item.second);
+                vts::CsConvertor csconv(ni.node.srs, node.srs);
+
+                auto dstCorners(projectCorners(node, csconv, srcCorners));
+
+                // ignore tiles that cannot be transformed
+                if (dstCorners.empty()) { continue; }
+
+                // find best tile size
+                auto bta(bestTileArea(dstCorners));
+
+                // find such the closest tile to the best tile size
+                auto dstLocalLod(bestLod(node, bta));
+                auto dstLod(node.id.lod + dstLocalLod);
+
+                LOG(info3)
+                    << "Best tile area: " << bta << " -> LOD: " << dstLod
+                    << " (node's local LOD: " << dstLocalLod << ").";
+
+                // generate tile range from corners
+                auto tr(tileRange(node, dstLocalLod, dstCorners));
+                LOG(info4) << "tile range: " << tr;
+
+                rasterizeTiles(dstRf, node, dstLod, tr
+                               , [&](const vts::TileId &id)
+                {
+                    sourceInfo_[id].push_back(srcId);
+                    dstTi_.set(id, 1);
+                });
+            }
         });
-        return tiles;
-    } catch (OutOfTree) {}
 
-    // nothing
-    return {};
+        // clone dst tile index to valid tree and make it complete
+        validTree_ = vts::TileIndex
+            (vts::LodRange(0, dstTi_.maxLod()), &dstTi_);
+        validTree_.complete();
+    }
+
+    const vts::TileIndex* validTree() const { return &validTree_; }
+
+    const vts::TileId::list& source(const vts::TileId &tileId) const {
+        auto fsourceInfo(sourceInfo_.find(tileId));
+        if (fsourceInfo == sourceInfo_.end()) { return emptySource_; }
+        return fsourceInfo->second;
+    }
+
+private:
+    SourceInfo sourceInfo_;
+    vts::TileIndex dstTi_;
+    vts::TileIndex validTree_;
+
+    static vts::TileId::list emptySource_;
+};
+
+// keep emtpy!
+vts::TileId::list SourceInfoBuilder::emptySource_;
+
+class Encoder : public vts::Encoder {
+public:
+    Encoder(const boost::filesystem::path &path
+            , const vts::TileSetProperties &properties
+            , vts::CreateMode mode
+            , const vts::TileSet &input
+            , const Config &config)
+        : vts::Encoder(path, properties, mode)
+        , config_(config), input_(input), srcRf_(input_.referenceFrame())
+        , srcInfo_(input_, referenceFrame())
+    {
+        setConstraints(Constraints().setValidTree(srcInfo_.validTree()));
+    }
+
+private:
+    virtual TileResult
+    generate(const vts::TileId &tileId, const vts::NodeInfo &nodeInfo
+             , const TileResult&)
+        UTILITY_OVERRIDE;
+
+    virtual void finish(vts::TileSet&) UTILITY_OVERRIDE {}
+
+    const Config config_;
+
+    const vts::TileSet &input_;
+    const vr::ReferenceFrame srcRf_;
+
+    SourceInfoBuilder srcInfo_;
+};
+
+vts::SubMesh warp(const vts::CsConvertor &conv, const vts::SubMesh &sm)
+{
+    (void) sm;
+    (void) conv;
+
+    vts::SubMesh out;
+
+    // TODO: implement me
+
+    return out;
 }
 
 Encoder::TileResult
 Encoder::generate(const vts::TileId &tileId, const vts::NodeInfo &nodeInfo
                   , const TileResult&)
 {
-    /** Current implementation:
-     *
-     *  For each source space division NODE:
-     *
-     *  1) Destination tile is projected into NODE's SRS SRS. All four corners
-     *     must fit inside NODE's root extents to create a quadrilateral Q.
-     *
-     *  2) Appropriate source LOD is selected by finding tile that has closest
-     *     area to area of Q.
-     *
-     *  3) Q is sampled in source tile space at select LOD: bounding box of Q is
-     *     converted to tile range and all tiles in that range that overlap with
-     *     Q are inserted into list of source tiles SRC.
-     *
-     *  4) All tiles in SRC must have NODE as their root nodes.
-     *
-     *  First valid result from the above algorithm is taken used as destination
-     *  tile's input.
-     */
-
-    std::vector<vts::TileId> srcTiles;
-
-    // grab tile corners
-    const auto &dstExtents(nodeInfo.node.extents);
-    const math::Points2 dstCorners = {
-        ul(dstExtents), ur(dstExtents), lr(dstExtents), ll(dstExtents)
-    };
-
-    // for each system in source reference frame
-    for (const auto &item : srcRf_.division.nodes) {
-        const auto &node(item.second);
-        vts::CsConvertor csconv(nodeInfo.node.srs, node.srs);
-
-        // convert them all to src SRS
-        math::Points2 srcCorners([&]() -> math::Points2
-        {
-            math::Points2 srcCorners;
-            try {
-                for (const auto &dstCorner : dstCorners) {
-                    srcCorners.push_back(csconv(dstCorner));
-                    LOG(info4) << std::fixed << "corner: "
-                               << dstCorner << " -> "
-                               << srcCorners.back();
-                    if (!inside(node.extents, srcCorners.back())) {
-                        // projected dst tile cannot fit inside this node's
-                        // extents -> ignore
-                        return {};
-                    }
-                }
-            } catch (std::exception) {
-                // whole tile cannot be projected -> ignore
-                return {};
-            }
-
-            // OK, we could convert whole tile into this reference system
-            return srcCorners;
-        }());
-
-        // ignore tiles that cannot be transformed
-        if (srcCorners.empty()) { continue; }
-
-        // find best tile size
-        auto bta(bestTileArea(srcCorners));
-
-        // find such the closest tile to the best tile size
-        auto srcLocalLod(bestLod(node, bta));
-        auto srcLod(node.id.lod + srcLocalLod);
-
-        LOG(info3) << "Best tile area: " << bta << " -> LOD: " << srcLod
-                   << " (node's local LOD: " << srcLocalLod << ").";
-
-        // generate tile range from corners
-        auto tr(tileRange(node, srcLocalLod, srcCorners));
-        LOG(info4) << "tile range: " << tr;
-
-        srcTiles = rasterTiles(referenceFrame(), node, srcLod, tr);
-        if (!srcTiles.empty()) { break; }
+    const auto &src(srcInfo_.source(tileId));
+    if (src.empty()) {
+        return TileResult::Result::noDataYet;
     }
 
-    if (srcTiles.empty()) { return TileResult::Result::noDataYet; }
+    LOG(info4) << "Source tiles(" << src.size() << "): "
+               << utility::join(src, ", ") << ".";
 
-    LOG(info4) << "Source tile count: " << srcTiles.size() << ".";
+    struct Source {
+        vts::TileId tileId;
+        vts::TileSource source;
 
-    return TileResult::Result::noDataYet;
+        Source(const vts::TileId &tileId, const vts::TileSet &ts)
+            : tileId(tileId), source(ts.getTileSource(tileId))
+        {}
 
-    (void) tileId;
+        typedef std::vector<Source> list;
+    };
+
+    Source::list sources;
+    for (const auto &srcId : src) {
+        UTILITY_OMP(critical)
+            sources.emplace_back(srcId, input_);
+    }
+
+    vts::CsConvertor srcPhy2Sds(srcRf_.model.physicalSrs, nodeInfo.node.srs);
+
+    Encoder::TileResult result;
+    vts::Mesh &mesh(*(result.tile().mesh = std::make_shared<vts::Mesh>()));
+
+    for (const auto &source : sources) {
+        auto srcMesh(vts::loadMesh(source.source.mesh));
+        for (const auto &srcSm : srcMesh) {
+            auto dstSm(warp(srcPhy2Sds, srcSm));
+            // TODO: generate external tx coordinates
+            // TODO: clip mesh by tile extents
+            // TODO: grab atlas (if any)
+            // TODO: warp navtile
+
+            if (!dstSm.empty()) {
+                mesh.add(dstSm);
+            }
+        }
+    }
+
+    if (mesh.empty()) {
+        // hm, nothing generated... tell that there is nothing yet
+        return TileResult::Result::noDataYet;
+    }
+
+    // TODO: generate mesh mask (rasterize mesh)
+
+    // done:
+    return result;
 }
 
 int Vts2Vts::run()
