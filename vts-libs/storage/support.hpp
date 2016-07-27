32851a23ca0e0aefd556e7f90bf5bac35c333fb3
Vaclav Blazek <vaclav.blazek@citationtech.net>
2016-07-27 19:38:08 +0200

    added templated support files

--- vts-libs/storage/support.hpp
+++ vts-libs/storage/support.hpp
@@ -16,12 +16,23 @@
 namespace vadstena { namespace storage {
 
 struct SupportFile {
+    typedef std::map<std::string, SupportFile> Files;
+    typedef std::map<std::string, std::string> Vars;
+
     const unsigned char *data;
     std::size_t size;
     std::time_t lastModified;
     const char *contentType;
+    bool isTemplate;
 
-    typedef std::map<std::string, SupportFile> Files;
+    SupportFile(const unsigned char *data, std::size_t size
+                , std::time_t lastModified, const char *contentType
+                , bool isTemplate = false)
+        : data(data), size(size), lastModified(lastModified)
+        , contentType(contentType), isTemplate(isTemplate)
+    {}
+
+    std::string expand(const Vars *vars, const Vars *defaults) const;
 };
 
 } } // namespace vadstena::storage
