fe9efc32baa4ce7d54cbb2a828b5fcfd90ab3c95
Vaclav Blazek <vaclav.blazek@citationtech.net>
2017-04-20 13:05:23 +0200

    added licence info into all hpp/cpp files

--- vts-libs/vts/colors.incl.cpp
+++ vts-libs/vts/colors.incl.cpp
@@ -1,3 +1,28 @@
+/**
+ * Copyright (c) 2017 Melown Technologies SE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * *  Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
     { 0x00, 0x00, 0x00 }, { 0xFF, 0xFF, 0xFF }, { 0x41, 0x89, 0x00 }, { 0xA6, 0x6F, 0x00 }
     , { 0x59, 0x00, 0xA3 }, { 0xFF, 0xE6, 0x1C }, { 0xFF, 0x34, 0xFF }, { 0x46, 0x4A, 0xFF }
     , { 0xE5, 0xDB, 0xFF }, { 0x00, 0x49, 0x7A }, { 0xA6, 0x00, 0x00 }, { 0xAC, 0xFF, 0x63 }
