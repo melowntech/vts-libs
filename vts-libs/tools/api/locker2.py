c2be8f67a9b9bac16497afc7a3cbafc1e6164a6a
Ladislav Horký <ladislav.horky@citationtech.net>
2017-04-20 11:15:23 +0200

    Added MarioDBlocker to locker2

--- vts-libs/tools/api/locker2.py
+++ vts-libs/tools/api/locker2.py
@@ -6,8 +6,14 @@ import fcntl
 import os
 import select
 import time
+import MySQLdb
+import stat
+from ConfigParser import ConfigParser
+import hashlib
 
 import dbglog as log
+
+_lock_timeout = 60
 
 def _makeNonblocking(fd):
     flags = fcntl.fcntl(fd, _fcntl.F_GETFL)
@@ -28,7 +34,7 @@ class Locker2:
         self._poll.register(self._in, select.EPOLLIN)
 
         # TODO: make configurable
-        self._renewPeriod = 5000
+        self._renewPeriod = _lock_timeout * 1000 / 2
         self._nextRenew = time.time() + self._renewPeriod
 
     def __call__(self):
@@ -160,9 +166,107 @@ class _ExampleLocker:
         for (lock, value) in self._locks.items():
             log.info3("Renewing lock <{}>.", lock)
 
+class _MarioDBLocker:
+    def __init__(self):
+        self._locks = {}
+
+        # load dbconf
+        conf = ConfigParser()
+        conf.read('locker.conf')
+
+        self._dbconf = {
+            'host' : conf.get('db','host'),
+            'user' : conf.get('db','user'),
+            'password' : conf.get('db','password'),
+            'database' : conf.get('db','database')
+        }
+
+    def _connect_to_db(self) :
+        db = MySQLdb.connect(host=self._dbconf['host'],
+                             user=self._dbconf['user'],
+                             passwd=self._dbconf['password'],
+                             db=self._dbconf['database'],
+                             charset='utf8')
+        return db
+
+    # concat storage dir inode and glue name
+    def _get_lock_name(self, gluename):
+        store_path = os.path.dirname(os.path.abspath(__file__))
+        store_inode = os.stat(os.path.realpath(store_path))[stat.ST_INO]
+
+        return (hashlib.md5(str(store_inode) + gluename).hexdigest()
+             + hashlib.sha1(str(store_inode) + gluename).hexdigest())
+
+    def lock(self, sublock):
+        lock_name = self._get_lock_name(sublock)
+
+        log.info3("Locking <{}>.", lock_name)
+        if (self._locks.get(lock_name) is not None):
+            raise ValueError, "Lock <%s> already locked." % (lock_name, )
+
+        db = self._connect_to_db()
+        cur = db.cursor()
+
+        lock_query = "CALL lock_acquire(%s, %s, %s, %s)"
+
+        cur.execute(lock_query, [ lock_name, _lock_timeout, None
+                                , socket.gethostname()])
+
+        token = cur.fetchall()[0][0]
+        cur.close()
+        db.commit()
+        db.close()
+
+        self._locks[lock_name] = token
+        return token
+
+    def renew(self):
+        db = self._connect_to_db()
+        cur = db.cursor()
+
+        lock_query = "CALL lock_acquire(%s, %s, %s, %s)"
+        host = socket.gethostname()
+
+        for (lock, value) in self._locks.items():
+            log.info3("Renewing lock <{}>.", lock)
+            cur.execute(lock_query, [ lock, _lock_timeout, value, host ])
+
+        cur.close()
+        db.commit()
+        db.close()
+
+    def unlock(self, sublock, value):
+        lock_name = self._get_lock_name(sublock)
+
+        log.info3("Unlocking <{}> (value: <{}>).", lock_name, value)
+        heldValue = self._locks.get(lock_name)
+        if (heldValue is None):
+            raise KeyError, "Lock <%s> not held." % (lock_name, )
+        if (heldValue != value):
+            raise ValueError, "Lock <%s> value mismatch." % (lock_name, )
+
+        db = self._connect_to_db()
+        cur = db.cursor()
+
+        lock_query = "CALL lock_release(%s, %s)"
+
+        cur.execute(lock_query, [lock_name, heldValue])
+
+        res = cur.fetchall()[0][0]
+
+        cur.close()
+        db.commit()
+        db.close()
+
+        if res != 'true':
+            raise Exception, "Cannot release lock <%s> in DB." % (lock_name, )
+
+        del self._locks[lock_name]
+
+
 if (__name__ == "__main__"):
     # Create simple locker and run it
     log.thread_id("locker2")
     log.info3("Locker2 starting.")
-    Locker2(impl = _ExampleLocker())()
+    Locker2(impl = _MarioDBLocker())()
     log.info3("Locker2 terminating.")
